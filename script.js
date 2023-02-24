925cddfaf327a17e6af0153697d8e82dbdd434be
Evelyn Victoria da Silva Santos <115710253+evelynvic23@users.noreply.github.com>
2023-02-24 11:58:54 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,103 @@
+
+const dados = '{"nome": "Yuri Alberto Monteiro da Silva", "idade": "21 anos", "profissao": "Jogador" }'
+
+const mostrar = JSON.parse(dados);
+
+
+function myfunction(){
+
+    document.getElementById("nome").innerHTML = "Nome: " + mostrar.nome;
+
+    document.getElementById("idade").innerHTML = "Idade: " + mostrar.idade;
+    
+    document.getElementById("profissao").innerHTML = "Profissão: " + mostrar.profissao;
+
+
+}
+
+
+
+const dados2 = '{"nome2": "Róger Guedes", "idade2": "26 anos", "profissao2": "Jogador" }'
+
+const mostrar2 = JSON.parse(dados2);
+
+
+function myfunction2(){
+
+    document.getElementById("nome2").innerHTML = "Nome: " + mostrar2.nome2;
+
+document.getElementById("idade2").innerHTML = "Idade: " + mostrar2.idade2;
+
+document.getElementById("profissao2").innerHTML = "Profissão: " + mostrar2.profissao2;
+
+}
+
+
+
+const dados3 = '{"nome3": "Cássio Roberto Ramos", "idade3": "35 anos", "profissao3": "Jogador" }'
+
+const mostrar3 = JSON.parse(dados3);
+
+
+function myfunction3(){
+
+    document.getElementById("nome3").innerHTML = "Nome: " + mostrar3.nome3;
+
+document.getElementById("idade3").innerHTML = "Idade: " + mostrar3.idade3;
+
+document.getElementById("profissao3").innerHTML = "Profissão: " + mostrar3.profissao3;
+
+}
+
+
+const dados4 = '{"nome4": "Fagner Conserva Lemos", "idade4": "33 anos", "profissao4": "Jogador" }'
+
+const mostrar4 = JSON.parse(dados4);
+
+
+function myfunction4(){
+
+    document.getElementById("nome4").innerHTML = "Nome: " + mostrar4.nome4;
+
+document.getElementById("idade4").innerHTML = "Idade: " + mostrar4.idade4;
+
+document.getElementById("profissao4").innerHTML = "Profissão: " + mostrar4.profissao4;
+
+}
+
+
+
+
+const dados5 = '{"nome5": "Ángel Rodrigo Romero", "idade5": "30 anos", "profissao5": "Jogador" }'
+
+const mostrar5 = JSON.parse(dados5);
+
+
+function myfunction5(){
+
+    document.getElementById("nome5").innerHTML = "Nome: " + mostrar5.nome5;
+
+document.getElementById("idade5").innerHTML = "Idade: " + mostrar5.idade5;
+
+document.getElementById("profissao5").innerHTML = "Profissão: " + mostrar5.profissao5;
+
+}
+
+
+const dados6 = '{"nome6": "José Paulo Bezerra Maciel Júnior", "idade6": "34 anos", "profissao6": "Jogador" }'
+
+const mostrar6 = JSON.parse(dados6);
+
+
+function myfunction6(){
+
+    document.getElementById("nome6").innerHTML = "Nome: " + mostrar6.nome6;
+
+document.getElementById("idade6").innerHTML = "Idade: " + mostrar6.idade6;
+
+document.getElementById("profissao6").innerHTML = "Profissão: " + mostrar6.profissao6;
+
+}
+
+
+
